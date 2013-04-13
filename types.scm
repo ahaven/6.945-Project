5ff95dae449677ed23f7e2d409f2a684e09870ee
Drew Haven <ahaven@mit.edu>
2013-04-13 15:06:27 -0400

    Added <type> class and -> shorthand.

--- types.scm
+++ types.scm
@@ -1,15 +1,15 @@
-(define (tagged-list? l tag)
-  (and (pair? l)
-       (= (car l) tag)))
-
-(define (all? l) (reduce and #t l))
+(define (all? l) (reduce boolean/and #t l))
 
 (define (type? t)
   (tagged-list? t 'type))
 
+(define <type>
+  (make-class '<type> '() '()))
+(declare-type-tester type? <type>)
+
 ;; Make a type representing one or more primitive types
 (define (type:make . symbols)
-  (if (there-exists? (lambda (s) (not (symbol? s))) symbols)
+  (if (there-exists? symbols (lambda (s) (not (symbol? s))))
       (error "Can't make a primitive type out of non-symbol" symbols))
   `(type ,(apply set:make symbols)))
 
@@ -19,11 +19,12 @@
 ;; Query whether this type is a set of primitive types
 (define (primitive-type? type)
   (and (type? type) (set? (type-symbols type))))
+(declare-explicit-guard primitive-type? (guard <type> primitive-type?))
 
 ;; Make a type representing a function
 ;; Requires inputs to be a list of types and output to be a type
 (define (type:function inputs output)
-  (if (there-exists? (lambda (t) (not (type? t))) inputs)
+  (if (there-exists? inputs (lambda (t) (not (type? t))))
       (error ("Function input type that isn't a type" inputs)))
   (if (not (type? output))
       (error ("Function output type that isn't a type" output)))
@@ -39,6 +40,7 @@
 (define (function-type? t)
   (and (type? t)
        (tagged-list? (cadr t) 'function)))
+(declare-explicit-guard function-type? (guard <type> function-type?))
 
 ;; Equality between types
 (define type:= equal?)
@@ -46,10 +48,12 @@
 ;; Type that is the upper bound of all types
 (define type:any '(type any))
 (define (type:any? t) (type:= t type:any))
+(declare-explicit-guard type:any? (guard <type> type:any?))
 
 ;; Type that is the lower bound of all types
-(define type:none '(type (set:make)))
+(define type:none `(type ,(set:make)))
 (define (type:none? t) (type:= t type:none))
+(declare-explicit-guard type:none? (guard <type> type:none?))
 
 ;; This is a partial order on types
 (define type:<=
@@ -118,14 +122,14 @@
 
 ;; Union of multiple types
 (define (type:union . types)
-  (reduce type:binary-union none types))
+  (reduce type:binary-union type:none types))
 
 ;; The intersection of two types is the greatest lower bound 
 (define type:binary-intersection
   (make-generic-operator 2 'type:binary-intersection
     (lambda (t1 t2)
       (if (and (type? t1) (type? t2))
-          none
+          type:none
           (error "Can't intersect non-types")))))
 
 (defhandler type:binary-intersection
@@ -176,4 +180,30 @@
 (defhandler type-of
   (lambda (exp) (type:make 'string))
   string?)
+
+;; We drill down and have two types representing boolean (t and f) because
+;; of the common pattern of returning some value or #f
+(defhandler type-of
+  (lambda (exp)
+    (if exp
+        (type:make 'bool:t)
+        (type:make 'bool:f)))
+  boolean?)
+;; The type of booleans is then the set of these two types
+(define type:boolean (type:make 'bool:t 'bool:f))
+
+;; Shorthand for writing function types. The last argument specified is
+;; the function's return type. Symbolic arguments will be boxed into types
+;; with type:make.
+(define (-> . args)
+  (if (null? args)
+      type:none
+      (let ((types
+             (map (lambda (t)
+                    (cond ((type? t) t)
+                          ((symbol? t) (type:make t))
+                          (else
+                           (error "Don't know how to type-ify" t))))
+                  args)))
+        (type:function (except-last-pair types) (car (last-pair types))))))
 
