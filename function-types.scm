d4bfad845114280f3af225adb9eebfd9ff5b68a1
Drew Haven <ahaven@mit.edu>
2013-05-04 18:00:38 -0400

    Renamed new-function-cell to new-procedure-cell.

--- function-types.scm
+++ function-types.scm
@@ -1,18 +1,4 @@
-(define (new-function-cell proc env)
-  (vector (make-cell) proc env))
-(define (function-cell fc) (vector-ref fc 0))
-(define (function-proc fc) (vector-ref fc 1))
-(define (function-env fc) (vector-ref fc 2))
-
-;(define-generic type:function-cell (object))
-;(define-generic type:function-proc (object))
-;(define-generic type:function-env (object))
-;(define-class (<function-cell> (constructor (cell proc env))) ()
-;  (cell accessor type:function-cell)
-;  (proc accessor type:function-proc)
-;  (env accessor type:function-env))
-
-;(define (new-function-cell proc env) (make-function-cell (make-cell) proc env))
-
-;(define (function-cell? t) (subclass? (object-class t) <function-cell>))
-;(declare-type-tester function-cell? <function-cell>)
+(define (new-procedure-cell proc)
+  (vector (make-cell) proc))
+(define (procedure-cell fc) (vector-ref fc 0))
+(define (procedure-proc fc) (vector-ref fc 1))
