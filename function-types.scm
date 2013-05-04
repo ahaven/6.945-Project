eb12ceb8e34fea23c599938a894dd3ef86a71438
katfang <justacopy@gmail.com>
2013-05-04 18:15:41 -0400

    renmaed some more things beause clobering

--- function-types.scm
+++ function-types.scm
@@ -1,4 +1,4 @@
 (define (new-procedure-cell proc)
   (vector (make-cell) proc))
-(define (procedure-cell fc) (vector-ref fc 0))
-(define (procedure-proc fc) (vector-ref fc 1))
+(define (get-procedure-cell fc) (vector-ref fc 0))
+(define (get-procedure-proc fc) (vector-ref fc 1))
