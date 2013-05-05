8053ac1929610fef3b035da28c3f59f9f8a1802c
Drew Haven <ahaven@mit.edu>
2013-05-05 19:17:17 -0400

    Fix unbuilt-procedure? predicate.

--- unbuilt-procedure.scm
+++ unbuilt-procedure.scm
@@ -1,11 +1,9 @@
 (define (new-unbuilt-procedure proc)
-  (vector (make-cell) proc))
-(define (procedure-cell fc) (vector-ref fc 0))
-(define (procedure-proc fc) (vector-ref fc 1))
+  (vector 'unbuilt-procedure (make-cell) proc))
+(define (procedure-cell fc) (vector-ref fc 1))
+(define (procedure-proc fc) (vector-ref fc 2))
 
-; todo: fix me, I'm a hack.
 (define (unbuilt-procedure? proc)
-  (if (and (vector? proc)
-           (= (length proc) 2))
-      #t
-      #f))
+  (and (vector? proc)
+       (= (vector-ref proc 0))
+       (= (vector-length proc) 3)))
